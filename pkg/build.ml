bbbec4c52b0f7ba5f80d37bcec5cb7f8093c7844
Daniel Bünzli <daniel.buenzli@erratique.ch>
2015-10-15 20:41:29 +0100

    Update topkg support.

--- pkg/build.ml
+++ pkg/build.ml
@@ -1,17 +1,17 @@
-#!/usr/bin/env ocaml 
+#!/usr/bin/env ocaml
 #directory "pkg";;
 #use "topkg.ml";;
 
-let () = 
-  let builder = `Other ("ocamlbuild -classic-display", "_build") in
+let () =
+  let builder = `OCamlbuild_no_ocamlfind [] in
   Pkg.describe "cmdliner" ~builder [
     Pkg.lib "pkg/META";
     Pkg.lib ~exts:Exts.module_library "src/cmdliner";
     Pkg.doc "README.md";
     Pkg.doc "CHANGES.md";
     Pkg.doc "test/chorus.ml";
-    Pkg.doc "test/cp_ex.ml"; 
-    Pkg.doc "test/darcs_ex.ml"; 
-    Pkg.doc "test/revolt.ml"; 
-    Pkg.doc "test/rm_ex.ml"; 
+    Pkg.doc "test/cp_ex.ml";
+    Pkg.doc "test/darcs_ex.ml";
+    Pkg.doc "test/revolt.ml";
+    Pkg.doc "test/rm_ex.ml";
     Pkg.doc "test/tail_ex.ml"; ]
