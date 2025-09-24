fe9af1d9d585512600397ac721a117a3d36b7e60
Daniel Bünzli <daniel.buenzli@erratique.ch>
2025-09-25 00:00:11 +0200

    Fix topkg distrib.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -6,7 +6,12 @@ open Topkg
 (* This is only here for `topkg distrib`. Remove once
    we switch to `b0 -- .release` *)
 
+let distrib =
+  (* The default removes Makefile *)
+  let exclude_paths () = Ok [".git";".gitignore";".gitattributes";"_build"] in
+  Pkg.distrib ~exclude_paths ()
+
 let () =
   let opams = [Pkg.opam_file "cmdliner.opam"] in
-  Pkg.describe "cmdliner" ~opams @@ fun c ->
+  Pkg.describe "cmdliner" ~distrib ~opams @@ fun c ->
   Ok [ Pkg.mllib ~api:["Cmdliner"] "src/cmdliner.mllib" ]
